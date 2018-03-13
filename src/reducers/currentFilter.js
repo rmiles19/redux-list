a35f9cc1f7f7ebcc0b6a8305eb1b0636d0e4c7a7
Rachel Miles <milesrachel9@gmail.com>
2018-03-13 14:22:42 -0600

    more changes with bugs

---
+++ src/reducers/currentFilter.js
@@ -0,0 +1,8 @@
+const currentFilter = ( state = 'All', action ) => {
+  switch ( action.type) {
+    default: 
+      return state;
+  }
+}
+
+export default currentfilter;
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,6 +1,16 @@
-const todos = ( state = [], action) => {
+const todos = ( state = [], action ) => {
   switch (action.type) {
-    default: 
+    case 'ADD_TODO':
+      //{type: 'ADD_TODO', todo: 'Buy Milk' }
+      return [action.todo, ...state]
+    case 'TOGGLE_TODO':
+      // { type: 'TOGGLE_TODO', id: 7 }
+      return state.map( todo => {
+        if (todo.id === action.id)
+          return {...todo, complete: !todo.complete}
+        return todo
+      })
+    default:
       return state;
   }
 }
